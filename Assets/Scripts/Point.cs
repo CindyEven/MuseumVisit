43c7ed908837e8ad397d9c9484a6471886288f55
CindyEven <evencindy@hotmail.fr>
2014-10-12 15:44:44 +0200

    moved materials folder

--- Assets/Scripts/Point.cs
+++ Assets/Scripts/Point.cs
@@ -4,12 +4,16 @@ using System.Collections.Generic;
 
 public class Point : MonoBehaviour {
 
+	[SerializeField]
 	public Point[] ConnectedTo;
+	[SerializeField]
 	public Point parent;
+	[SerializeField]
+	public Vector3 pointPosition;
 
 	// Use this for initialization
 	void Start () {
-
+		pointPosition = transform.position;
 	}
 	
 	// Update is called once per frame

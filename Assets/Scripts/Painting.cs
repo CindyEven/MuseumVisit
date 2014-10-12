1a3f147c1337fc4427a51883ea62b4d08fc3ae56
CindyEven <evencindy@hotmail.fr>
2014-10-12 21:00:30 +0200

    show informations about the painting

---
+++ Assets/Scripts/Painting.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Xml;
+using System.Xml.Serialization;
+
+public class Painting : MonoBehaviour {
+
+	[XmlAttribute("name")]
+	public string name;
+	public Vector3 position;
+	public Quaternion rotation;
+	public Vector3 scale;
+	public string artist;
+	public string paintingName;
+	public int date;
+
+	// Use this for initialization
+	void Start () {
+//		Painting[] paintingsCollection = AutoInstanciation.Load("C:/Users/Cindy/Documents/test/MuseumVisit/Assets/Resources/paintingList.xml");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/ShowPaintingInfo.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShowPaintingInfo : MonoBehaviour {
+
+	private bool showGUI = false;
+	private string text;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		RaycastHit hit;
+		Ray ray = GetComponentInChildren<Camera>().camera.ScreenPointToRay(Input.mousePosition);
+		
+		if (Physics.Raycast(ray, out hit)) {
+			Transform objectHit = hit.transform;
+			if (objectHit.GetComponent<Painting>()&& Vector3.Distance(transform.position,objectHit.transform.position)<3.0f){
+				Debug.Log("Artist : "+objectHit.GetComponent<Painting>().artist);
+				text = "Artiste : "+objectHit.GetComponent<Painting>().artist + "\nOeuvre : " + objectHit.GetComponent<Painting>().paintingName + "\nAnnée : "+objectHit.GetComponent<Painting>().date;
+				showGUI = true;
+			}else{
+				showGUI = false;
+			}
+		}
+	}
+
+	void OnGUI(){
+		if (showGUI) {
+			GUI.Box (new Rect (Screen.width - 300,Screen.height - 100,250,50), text);
+		}
+	}
+}

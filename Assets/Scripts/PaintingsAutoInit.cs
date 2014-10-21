3d39b9c56eed8342d51809720fbe38b85247d247
CindyEven <evencindy@hotmail.fr>
2014-10-21 18:27:43 +0200

    added comments to some Scripts

--- Assets/Scripts/PaintingsAutoInit.cs
+++ Assets/Scripts/PaintingsAutoInit.cs
@@ -5,6 +5,7 @@ using System.Xml.Linq;
 
 public class PaintingsAutoInit : MonoBehaviour {
 	void Awake(){
+		// Reset and rename the GameObject as soon as we attach this Scrip to it
 		transform.position = Vector3.zero;
 		transform.rotation = Quaternion.identity;
 		transform.localScale = Vector3.one;

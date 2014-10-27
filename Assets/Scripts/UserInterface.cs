7e63ddcd716be4228f15d9aa6dcaf1b9e28d584d
CindyEven <evencindy@hotmail.fr>
2014-10-27 15:59:43 +0000

    Options interface for the second Scene

--- Assets/Scripts/UserInterface.cs
+++ Assets/Scripts/UserInterface.cs
@@ -6,22 +6,25 @@ public class UserInterface : MonoBehaviour {
 	public Camera camVisitor;
 	public Camera camGlobal;
 
-	public Rect windowRect1 = new Rect (20, 50, 250, 180);
-	public Rect windowRect2 = new Rect (20, 20, 120, 100);
+	private Rect windowRect1 = new Rect (20, 50, 250, 180);
+	private Rect windowRect2 = new Rect (20, 50, 250, 310);
+	public Rect windowRect3a = new Rect (20, 50, 250, 310);
+//	private Rect windowRect3b = new Rect (20, 50, 250, 310);
+
 
 	private Painting paintingHit;
 	private float rayDist;
 
 	private int algoSelected = 1;
+	private float seekCoef = 1.25f, sepCoef = 1.20f, cohesionCoef = 0.60f, alignCoef = 1.0f;
 
 	private bool showOptions = false;
 	private bool visitorPOV = true;
 	private bool showInfoPainting = false;
-	private bool canSelectPainting = false;
 	
 	void Update () {
-		// If I press the Space Key, we stop the movement of the camera and show the GUI Window with the options
-		// and when we repress it, it does the oposit
+		// On Space key pressed, the movement of the camera is stopped and the GUI Window with the options appears
+		// If the key is pressed again it does the opposite
 		if (Input.GetKeyDown (KeyCode.Space)) {
 			if (!showOptions) {
 				camVisitor.GetComponent<MouseLook> ().enabled = false;
@@ -56,7 +59,7 @@ public class UserInterface : MonoBehaviour {
 				paintingHit = hit.transform.GetComponent<Painting>();
 				if(Input.GetMouseButtonDown(0)){
 					int caseSwitch = Application.loadedLevel;
-					switch (caseSwitch)
+					switch (caseSwitch) // For the scene 1 and 2 we can select the paiting 'destination' by clicking on it
 					{
 						case 1:
 							AgentSimpleBehaviour.setTargetPainting(paintingHit);
@@ -82,12 +85,13 @@ public class UserInterface : MonoBehaviour {
 			switch (caseSwitch)
 			{
 			case 1: // Scene_q1
-				canSelectPainting = true;
 				windowRect1 = GUI.Window (0, windowRect1, OptionsWindowFunction1, "Options");
 				break;
 			case 2: // Scene_q2
-				canSelectPainting = true;
 				windowRect2 = GUI.Window (0, windowRect2, OptionsWindowFunction2, "Options");
+				break;
+			case 3: // Scene_q3a
+				windowRect3a = GUI.Window (0, windowRect3a, OptionsWindowFunction3a, "Options");
 				break;
 			default:
 				//Console.WriteLine("Default case");
@@ -100,8 +104,46 @@ public class UserInterface : MonoBehaviour {
 		}
 
 	}
-
+	
 	void OptionsWindowFunction1 (int windowID) {
+		GlobalOptions ();
+		GUI.Label (new Rect (10, 95, 240, 50), "Vous pouvez choisir le tableau de destination de l'agent virtuel en cliquant dessus.");
+		string[] algoChoices = new string[] { " ", " ", " " };
+		Rect position = new Rect(10, 150, 230, 30);
+		algoSelected = GUI.SelectionGrid(position, algoSelected, algoChoices, algoChoices.Length+1, GUI.skin.toggle);
+		GUI.Label(new Rect(10,150,230,30),"     BFS        A*           A* sans lien");
+		AgentSimpleBehaviour.setAlgo (algoSelected);
+		GUI.DragWindow();
+	}
+
+	void OptionsWindowFunction2 (int windowID) {
+		GlobalOptions ();
+		GUI.Label (new Rect (10, 95, 240, 50), "Vous pouvez choisir le tableau de destination de l'agent virtuel en cliquant dessus.");
+		string[] algoChoices = new string[] { " ", " ", " " };
+		Rect position = new Rect(10, 150, 230, 30);
+		algoSelected = GUI.SelectionGrid(position, algoSelected, algoChoices, algoChoices.Length+1, GUI.skin.toggle);
+		GUI.Label(new Rect(10,150,230,30),"     BFS        A*           A* sans lien");
+		AgentSimpleGroupBehaviour.setAlgo (algoSelected);
+		GUI.Label(new Rect(10,175,230,30),"Modification des coeficients :");
+		GUI.Label(new Rect(10,200,230,30),"Seek");
+		seekCoef = GUI.HorizontalSlider (new Rect (100, 205, 100, 30), seekCoef, 0.0f, 5.0f);
+		GUI.Label(new Rect(10,225,230,30),"Séparation");
+		sepCoef = GUI.HorizontalSlider (new Rect (100, 230, 100, 30), sepCoef, 0.0f, 5.0f);
+		GUI.Label(new Rect(10,250,230,30),"Cohésion");
+		cohesionCoef = GUI.HorizontalSlider (new Rect (100, 255, 100, 30), cohesionCoef, 0.0f, 5.0f);
+		GUI.Label(new Rect(10,275,230,30),"Alignement");
+		alignCoef = GUI.HorizontalSlider (new Rect (100, 280, 100, 30), alignCoef, 0.0f, 5.0f);
+		SteeringBehaviour.updateSteeringCoeff (seekCoef, sepCoef, cohesionCoef, alignCoef);
+		GUI.DragWindow();
+	}
+
+	void OptionsWindowFunction3a (int windowID) {
+		GlobalOptions ();
+		GUI.DragWindow();
+	}
+
+	// Elements of the options Window common for all the scenes :
+	void GlobalOptions(){
 		if(GUI.Button(new Rect(10, 20, 100, 30), "Menu Principal")) {
 			Application.LoadLevel("Scene_Menu");
 		}
@@ -116,20 +158,6 @@ public class UserInterface : MonoBehaviour {
 			}
 		}
 		GUI.Label (new Rect (120,65,100,25), "Touche A");
-		GUI.Label (new Rect (10, 95, 240, 50), "Vous pouvez choisir le tableau de destination de l'agent virtuel en cliquant dessus.");
-
-		string[] algoChoices = new string[] { " ", " ", " " };
-		Rect position = new Rect(10, 150, 230, 30);
-		algoSelected = GUI.SelectionGrid(position, algoSelected, algoChoices, algoChoices.Length+1, GUI.skin.toggle);
-		GUI.Label(new Rect(10,150,230,30),"     BFS        A*           A* sans lien");
-		AgentSimpleBehaviour.setAlgo (algoSelected);
-		GUI.DragWindow();
-	}
-
-	void OptionsWindowFunction2 (int windowID) {
-
-		AgentSimpleGroupBehaviour.setAlgo (algoSelected);
-		GUI.DragWindow();
 	}
 
 }

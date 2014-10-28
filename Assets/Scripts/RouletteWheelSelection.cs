80896b0fed9a4a70b141dfd96fd1e82d8d7b0241
GuillaumeBiannic <guillaume.biannic29@gmail.com>
2014-10-28 15:06:35 +0100

    new Script to select a painting

---
+++ Assets/Scripts/RouletteWheelSelection.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public static class RouletteWheelSelection {
+
+	static Painting[] listPainting = GameObject.FindObjectsOfType<Painting> ();
+
+	public static Painting getAPainting(){
+		float sumFitness = 0.0f;
+
+		foreach(Painting p in listPainting){
+			sumFitness += p.fitness;
+		}
+
+		float nb = Random.Range (0.0f, sumFitness);
+		float currentFitness = 0.0f;
+
+		foreach(Painting p in listPainting){
+			currentFitness += p.fitness;
+			if(nb < currentFitness){
+				return p;
+			}
+		}
+
+		return null;
+	}
+
+}

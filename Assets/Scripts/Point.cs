1333effc6f57202456f9f6f8198584e18709e46c
GuillaumeBiannic <guillaume.biannic29@gmail.com>
2014-11-15 22:27:42 +0100

    new attribut type

--- Assets/Scripts/Point.cs
+++ Assets/Scripts/Point.cs
@@ -4,10 +4,17 @@ using System.Collections.Generic;
 
 public class Point : MonoBehaviour {
 
+	public enum Type {
+		Passage,
+		Observation,
+		Attente
+	}
+
 	public List<Point> ConnectedTo;
 	public Point parent;
 	public float scoreG;
 	public float score;
+	public Type typePoint;
 
 	// Update is called once per frame
 	void Update () {

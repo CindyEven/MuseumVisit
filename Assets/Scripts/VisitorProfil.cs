f3b75264457b8d4f2cc38b7cb1679a66f0b01d84
CindyEven <evencindy@hotmail.fr>
2014-11-20 18:39:55 +0100

    create User Profile

---
+++ Assets/Scripts/VisitorProfil.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class VisitorProfil : MonoBehaviour {
+	public List<Painting.Tags> profil;
+	// Use this for initialization
+	public void updateFitness () {
+		Painting[] paintingList = GameObject.FindObjectsOfType <Painting>();
+		foreach( Painting.Tags t in profil){
+			foreach(Painting p in paintingList){
+				if (p.tagList.Contains(t)){
+					p.fitness += 500.0f;
+				}
+			}
+		}
+		AgentStateMachineBehavior[] agentList = GameObject.FindObjectsOfType <AgentStateMachineBehavior>();
+		foreach(AgentStateMachineBehavior a in agentList){
+			a.initFitness();
+		}
+	}
+
+}

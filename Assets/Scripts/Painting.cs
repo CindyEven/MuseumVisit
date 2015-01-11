0a9a91ef1a89d65fa6e5b5cdbb4fac00a3824e15
Guillaume Biannic <guillaume.biannic29@gmail.com>
2015-01-11 21:36:30 +0100

    new fct for visit3

--- Assets/Scripts/Painting.cs
+++ Assets/Scripts/Painting.cs
@@ -29,4 +29,14 @@ public class Painting : MonoBehaviour {
 	public float fitness = 1.0f;
 	public List<Tags> tagList;
 
+	public void IsLiked(){
+		Painting[] listPainting = GameObject.FindObjectsOfType <Painting>();
+		foreach (Tags t in tagList){
+			foreach(Painting p in listPainting){
+				if(p.tagList.Contains(t)){
+					p.fitness += 1.0f;
+				}
+			}
+		}
+	}
 }

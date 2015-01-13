dbb2be100fd5bd1508d3a0b95a6841ffa38fc64a
Guillaume Biannic <guillaume.biannic29@gmail.com>
2015-01-13 16:11:30 +0100

    few corrections

--- Assets/Scripts/AgentWithFitnessBehaviour.cs
+++ Assets/Scripts/AgentWithFitnessBehaviour.cs
@@ -4,88 +4,53 @@ using System.Collections.Generic;
 
 public class AgentWithFitnessBehaviour : MonoBehaviour {
 	
-	Point startPoint;
-	public Point targetPoint;
 	public Point nextDestination;
+	public Point targetPoint;
 	//	Point previousDestination;
 	
-	Painting targetPainting;
-	Painting[] listPainting;
+	public Painting targetPainting;
+	public Painting[] listPainting;
 	
-	List<GameObject> visiteurs;
-	List<GameObject> visiteursWithSameDestination;
+	public List<GameObject> visiteurs;
+	public List<GameObject> visiteursWithSameDestination;
 	
-	List<Point> path;
+	public Vector3 force;
+	public Vector3 direction;
 	
-	int indexPainting = 0;
-	int indexPath = 0;
+	public SteeringBehaviour steering;
 	
-	SteeringBehaviour steering;
-
-	Dictionary<Painting,float> paintingsFitness;
-
 	public float distArrive = 1.5f;
+	
+	public Dictionary<Painting,float> paintingsFitness;
+	
+	StateMachineFitness sm;
+	
 	// Use this for initialization
 	void Start () {
-		listPainting = GameObject.FindObjectsOfType <Painting>();
-
-		initFitness (listPainting);
+		
+		//targetPainting = RouletteWheelSelection.getAPainting2 (listPainting,paintingsFitness);
+		
 		steering = gameObject.GetComponent<SteeringBehaviour> ();
 		
-
-		//indexPainting = (int)Random.Range(0, listPainting.Length-1);
-		//targetPainting = listPainting [indexPainting];*/
-		targetPainting = RouletteWheelSelection.getAPainting2 (listPainting,paintingsFitness);
-		targetPoint = FindTheNearestPoint (targetPainting.gameObject);
-		startPoint = FindTheNearestPoint (gameObject);
-		path = AStar.search(startPoint, targetPoint);
-		nextDestination = path [indexPath];
-		
+		listPainting = GameObject.FindObjectsOfType <Painting>();
+		initFitness ();
 		visiteurs = new List<GameObject> ();
 		visiteursWithSameDestination = new List<GameObject> ();
+		
+		sm = new StateMachineFitness (this);
+		
+		force = Vector3.zero;
+		direction = Vector3.zero;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(isBlock()){
-			startPoint = FindTheNearestPoint (gameObject);
-			indexPath=0;
-			path = AStar.search(startPoint, targetPoint);
-			nextDestination = path [indexPath];
-		}
-		
-		Vector3 position = nextDestination.transform.position;
-		
-		Vector3 force = steering.getForce (gameObject, position, visiteurs, visiteursWithSameDestination);
-		Vector3 direction = steering.getDirection (gameObject);
+		sm.Execute ();
 		transform.LookAt (new Vector3(direction.x,1.0f,direction.z));
 		rigidbody.AddForce(new Vector3(force.x,0.0f,force.z));
-		
-		if((position - transform.position).sqrMagnitude < distArrive){
-			if(indexPath < path.Count-1){
-				
-				indexPath++;
-				//				previousDestination = nextDestination;
-				nextDestination = path [indexPath];
-				
-			}else{
-				
-				indexPath=0;
-				
-				/*indexPainting = (int)Random.Range(0, listPainting.Length-1);
-				targetPainting = listPainting [indexPainting];*/
-				targetPainting = RouletteWheelSelection.getAPainting2 (listPainting,paintingsFitness);
-				
-				startPoint = FindTheNearestPoint (gameObject);
-				targetPoint = FindTheNearestPoint (targetPainting.gameObject);
-				
-				path = AStar.search(startPoint, targetPoint); 
-				nextDestination = path [indexPath];
-			}
-		}
 	}
 	
-	Point FindTheNearestPoint(GameObject go){
+	public Point FindTheNearestPoint(GameObject go){
 		
 		Point[] listPoints = GameObject.FindObjectsOfType <Point>();
 		Point nearPoint = null;
@@ -105,7 +70,31 @@ public class AgentWithFitnessBehaviour : MonoBehaviour {
 		return nearPoint;
 	}
 	
-	bool isBlock(){
+	public Point FindTheNearestMeetingPoint(){
+		
+		GameObject go = gameObject;
+		Point[] listPoints = GameObject.FindObjectsOfType <Point>();
+		Point nearPoint = null;
+		float distMin = float.PositiveInfinity;
+		
+		foreach (Point p in listPoints) {
+			
+			if(p.typePoint == Point.Type.Attente){
+				
+				float dist = Vector3.Distance(p.transform.position,go.transform.position);
+				
+				if(dist < distMin && !Physics.Raycast (go.transform.position, p.transform.position - go.transform.position, dist)){
+					
+					distMin = dist;
+					nearPoint = p;
+				}
+			}
+		}
+		
+		return nearPoint;
+	}
+	
+	public bool isBlock(){
 		Vector3 pos = transform.position;
 		Vector3 destPos = nextDestination.transform.position;
 		
@@ -116,23 +105,33 @@ public class AgentWithFitnessBehaviour : MonoBehaviour {
 		
 		return false;
 	}
-
-	void initFitness(Painting[] list){
+	
+	public void initFitness(){
 		paintingsFitness = new Dictionary<Painting, float>();
-		foreach (Painting p in list) {
+		foreach (Painting p in listPainting) {
 			paintingsFitness.Add(p,p.fitness);
 		}
 	}
-
+	
+	public void updateFitness(){
+		foreach(Painting.Tags t in targetPainting.tagList){
+			foreach(Painting p in listPainting){
+				if(p.tagList.Contains(t)){
+					paintingsFitness[p] += 1.0f;
+				}
+			}
+		}
+	}
+	
 	void OnTriggerEnter(Collider other) {
-		if (other.gameObject.tag == "Agent" && !gameObject.Equals(other.gameObject)) {
+		if (other.gameObject.tag == "Agent" && !gameObject.Equals(other.gameObject) && isVisible(gameObject, other.gameObject)) {
 			
 			if(!visiteurs.Contains(other.gameObject)){
 				
 				visiteurs.Add (other.gameObject);
 			}
 			
-			AgentBehaviour a = other.gameObject.GetComponent<AgentBehaviour>();
+			AgentWithFitnessBehaviour a = other.gameObject.GetComponent<AgentWithFitnessBehaviour>();
 			
 			if(targetPoint == a.targetPoint || nextDestination == a.nextDestination){
 				
@@ -143,7 +142,7 @@ public class AgentWithFitnessBehaviour : MonoBehaviour {
 			}
 		}
 		
-		if (other.gameObject.tag == "Agent"){
+		if (other.gameObject.tag == "Player" && isVisible(gameObject, other.gameObject)){
 			
 			if(!visiteurs.Contains(other.gameObject)){
 				
@@ -164,5 +163,19 @@ public class AgentWithFitnessBehaviour : MonoBehaviour {
 			
 			visiteurs.Remove (other.gameObject);
 		}
+	}
+	
+	bool isVisible(GameObject me, GameObject target){
+		
+		Vector3 myPosition = me.transform.position;
+		Vector3 targetPosition = target.transform.position;
+		float dist = Vector3.Distance (targetPosition, myPosition);
+		
+		if(!Physics.Raycast (myPosition, targetPosition - myPosition, dist)){
+			
+			return true;
+		}
+		
+		return false;
 	}
 }

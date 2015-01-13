24d7a56e876b96568f1ccdce751f5797b060335d
Guillaume Biannic <guillaume.biannic29@gmail.com>
2015-01-13 16:14:17 +0100

    state machine for agent with his own fitness

---
+++ Assets/Scripts/StateMachineFitness.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StateMachineFitness {
+	
+	public AgentWithFitnessBehaviour agent;
+	States currentState;
+	
+	public StateMachineFitness(AgentWithFitnessBehaviour a){
+		agent = a;
+		ChangeState(new GoToFitness (this));
+	}
+	
+	public void ChangeState(States state){
+		currentState = state;
+		currentState.Enter ();
+	}
+	
+	public void Execute(){
+		currentState.Execute ();
+	}
+}
